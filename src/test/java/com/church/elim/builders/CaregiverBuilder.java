9d7aafcc6d86ab3c191e6cd530867cc844ca8c3b
Adrian Dolha <adolha@sdl.com>
2013-07-04 16:29:11 +0100

    Generic controller test for get, create, remove. Also added CaregiverBuilder.

---
+++ src/test/java/com/church/elim/builders/CaregiverBuilder.java
@@ -0,0 +1,35 @@
+package com.church.elim.builders;
+
+import com.church.elim.domain.Caregiver;
+import com.church.elim.domain.Children;
+import com.church.elim.domain.Person;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.church.elim.ElimRandom.newId;
+import static com.church.elim.ElimRandom.newName;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: adi
+ * Date: 5/15/13
+ * Time: 10:43 PM
+ */
+public class CaregiverBuilder {
+    public static CaregiverBuilder aCaregiver(){
+        return new CaregiverBuilder();
+    }
+    public Caregiver buildRandom(){
+        return null;
+    }
+
+    public Caregiver build() {
+        Person person = PersonBuilder.aPerson().build();
+        Caregiver caregiver =  new Caregiver();
+        caregiver.setPerson(person);
+        caregiver.setId((long) 1);
+        return caregiver;
+    }
+}
--- src/test/java/com/church/elim/controller/rest/RestDomainControllerTest.java
+++ src/test/java/com/church/elim/controller/rest/RestDomainControllerTest.java
@@ -3,22 +3,42 @@ package com.church.elim.controller.rest;
 import com.church.elim.ElimTest;
 import com.church.elim.builders.ChildrenBuilder;
 import com.church.elim.builders.PersonBuilder;
+import com.church.elim.controller.PersonController;
+import com.church.elim.domain.Caregiver;
+import com.church.elim.domain.Person;
 import com.church.elim.matchers.PersonMatcher;
+import com.church.elim.repository.PersonRepository;
+import com.church.elim.service.CaregiverService;
+import com.church.elim.service.CustomUserDetailsService;
+import com.church.elim.service.EntityDoesNotExistException;
+import com.church.elim.service.PersonService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.request.ServletWebRequest;
 
+import static com.church.elim.builders.CaregiverBuilder.aCaregiver;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 /**
@@ -32,20 +52,76 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 public class RestDomainControllerTest extends ElimTest{
     @Autowired
     RestCaregiverController categiverController;
-    private MockMvc mockMvc;
     PersonBuilder personBuilder = new PersonBuilder();
     ChildrenBuilder childrenBuilder = new ChildrenBuilder();
+    @Autowired
+    WebApplicationContext wac;
+    @Autowired
+    MockServletContext servletContext; // cached
+    @Autowired
+    MockHttpSession session;
+    @Autowired
+    MockHttpServletRequest request;
+    @Autowired
+    MockHttpServletResponse response;
+    @Autowired
+    ServletWebRequest webRequest;
+    @Autowired
+    private PersonController personController;
+
+    PersonService mockPersonService = Mockito.mock(PersonService.class);
+    PersonRepository mockPersonRepo = Mockito.mock(PersonRepository.class);
+    CaregiverService mockCaregiverService = Mockito.mock(CaregiverService.class);;
+    @Autowired
+    CustomUserDetailsService userService;
+    @Autowired
+    PersonRepository personRepo;
+    Person popIonel;
+    Caregiver aCaregiver = aCaregiver().build();
+    private MockMvc mockMvc;
+    private String entityName = "Caregiver";
+    private String resourceUrl = "/caregivers";
     @Before
     public void setup() {
+        popIonel = new PersonBuilder().buildWithChildren();
         mockMvc = webAppContextSetup(wac).build();
+        ReflectionTestUtils.setField(wac.getBean(RestCaregiverController.class), "domainService", mockCaregiverService);
+        when(this.mockCaregiverService.get(any(Long.class))).thenReturn(aCaregiver().build());
     }
     @Test
     public void testGet() throws Exception {
         long id = (long) 1;
-        ResultActions actions = mockMvc.perform(get("/caregivers/1")
+        ResultActions actions = mockMvc.perform(get(resourceUrl+"/1")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(1));
         System.out.println(actions.andReturn().getResponse().getContentAsString());
+    }
+
+    @Test
+    public void testCreatePerson() throws Exception{
+        when(this.mockCaregiverService.create(any(Caregiver.class))).thenReturn(aCaregiver);
+        String person = toJSON(aCaregiver);
+        System.out.println(person);
+        this.mockMvc.perform(post(resourceUrl)
+                .content(person)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", linkTo(RestCaregiverController.class).slash(aCaregiver.getId()).toUri().toString()));
+    }
+
+    @Test
+    public void testDeletePerson() throws Exception{
+        this.mockMvc.perform(delete(resourceUrl+"/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+    @Test
+    public void testDeletePersonNotFound() throws Exception{
+        Mockito.doThrow(new EntityDoesNotExistException(entityName, (long)2)).when(mockCaregiverService).remove((long) 2);
+        this.mockMvc.perform(delete(resourceUrl+"/2")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string(String.format(EntityDoesNotExistException.MESSAGE, entityName,2)));
     }
 }
